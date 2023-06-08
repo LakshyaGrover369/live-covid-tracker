a8acdd3a0263aa9db779e867dafede02ce983584
lakshyagrover369 <groverlakshya.25.lg@gmail.com>
2023-06-08 19:28:54 +0530

    final commit

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { ReactDOM } from "react";
 import './header.css';
 
 
@@ -11,30 +10,24 @@ const Header = () =>{
         const res= await fetch("https://data.covid19india.org/data.json");
         const actualData=await res.json();
 
-        // console.log(actualData.statewise[0]); 
+        console.log(actualData.statewise[0]); 
 
-        // const recovered=document.querySelector('#recovered');
-        // recovered.innerHTML=actualData.statewise[0].recovered;
+        const recovered=document.querySelector('#recovered');
+        recovered.innerHTML=actualData.statewise[0].recovered;
         
-        // const confirmed=document.querySelector('#confirmed');
-        // confirmed.innerHTML=actualData.statewise[0].confirmed;
+        const confirmed=document.querySelector('#confirmed');
+        confirmed.innerHTML=actualData.statewise[0].confirmed;
 
-        // const deaths=document.querySelector('#death');
-        // deaths.innerHTML=actualData.statewise[0].deaths;
+        const deaths=document.querySelector('#death');
+        deaths.innerHTML=actualData.statewise[0].deaths;
 
-        // const active=document.querySelector('#active');
-        // active.innerHTML=actualData.statewise[0].active;
+        const active=document.querySelector('#active');
+        active.innerHTML=actualData.statewise[0].active;
 
         
-        // const timeUpdated=document.querySelector('#timeUpdated');
-        // timeUpdated.innerHTML=actualData.statewise[0].lastupdatedtime
+        const timeUpdated=document.querySelector('#timeUpdated');
+        timeUpdated.innerHTML=actualData.statewise[0].lastupdatedtime
 
-        ReactDOM.render(<>{actualData.statewise[0].recovered}</>,document.querySelector('#recovered'));
-        ReactDOM.render(<>{actualData.statewise[0].confirmed}</>,document.querySelector('#confirmed'));
-        ReactDOM.render(<>{actualData.statewise[0].deaths}</>,document.querySelector('#death'));
-        ReactDOM.render(<>{actualData.statewise[0].active}</>,document.querySelector('#active'));
-        ReactDOM.render(<>{actualData.statewise[0].lastupdatedtime}</>,document.querySelector('#timeUpdated'));
-        
       }
       catch(err){
         console.log(err);
@@ -52,7 +45,7 @@ const Header = () =>{
       <div className="main">
 
         <h1 id="heading">LIVE COVID TRACKER</h1>
-        <marquee id="attract"  direction="right" >🔴LIVE </marquee>
+        <div className="marque" >🔴LIVE </div>
         <ul>
         
           <li>
@@ -70,7 +63,7 @@ const Header = () =>{
                 <div className="card_main">
                   <p>TOTAL </p>
                   <span>RECOVERED</span>                    
-                  <h1 class="data" id="recovered"></h1>
+                  <h1 class="data" id="recovered">#</h1>
                 </div>
             </div>
           </li>
@@ -80,7 +73,7 @@ const Header = () =>{
                 <div className="card_main">
                   <p>TOTAL </p>
                     <span>CONFIRMED</span>
-                  <h1 class="data" id="confirmed"></h1>
+                  <h1 class="data" id="confirmed">#</h1>
                 </div>
             </div>
           </li>
@@ -90,7 +83,7 @@ const Header = () =>{
                 <div className="card_main">
                   <p>TOTAL </p>
                   <span>DEATH</span>
-                  <h1 class="data" id="death"></h1>
+                  <h1 class="data" id="death">#</h1>
                 </div>
             </div>
           </li>
@@ -100,7 +93,7 @@ const Header = () =>{
                 <div className="card_main">
                   <p>TOTAL </p>
                   <span>ACTIVE</span>
-                  <h1 class="data" id="active"></h1>
+                  <h1 class="data" id="active">#</h1>
                 </div>
             </div>
           </li>
@@ -110,7 +103,7 @@ const Header = () =>{
                 <div className="card_main">
                   <p>LAST </p>
                   <span>UPDATED</span>
-                  <h1 class="data" id="timeUpdated"></h1>
+                  <h1 class="data" id="timeUpdated">#</h1>
                 </div>
             </div>
           </li>
